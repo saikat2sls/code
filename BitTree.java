4c7c20b8e09c656f7a29de5b11172cd7cd626870
saikat2sls <59751934+saikat2sls@users.noreply.github.com>
2021-04-07 09:33:48 -0700

    Create BitTree.java

---
+++ BitTree.java
@@ -0,0 +1,22 @@
+/**
+ *   bit tree representation where each node is either a 0 or 1
+ *
+ *             1
+ *            / \
+ *           1   0
+ *          /\   /\
+ *         1  1 0  1
+ *
+ *     the tree follow the rules:
+ *     a. node is 1 only all the children are 1 else 0
+ *     b. tree is a complete binary tree so can be represented as an array
+ *        e.g. [1,1,0,1,1,0,1]
+ *
+ *     operations:
+ *     1. update(index,length,0/1)
+ *     2. getBit(index)
+ */
+
+
+public class BitTree {
+}
